1c448e6a770dfa006e17715c6216f6efa3f98b7b
kawaus03 <pf.ctm.box@gmail.com>
2022-05-26 18:03:39 +0900

    fix: cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost'))
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost')),
 
     'allowed_origins_patterns' => [],
 
